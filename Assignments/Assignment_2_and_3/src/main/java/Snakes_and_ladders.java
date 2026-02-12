26d58ebfef3d6d54836f77a713c7bba68350ad5d
lakira23 <lakirar2020@gmail.com>
2026-02-12 14:34:40 +0000

    start on assignment 3, created a basic structure for the classes

--- Assignments/Assignment_2_and_3/src/main/java/Snakes_and_ladders.java
+++ Assignments/Assignment_2_and_3/src/main/java/Snakes_and_ladders.java
@@ -1,8 +1,60 @@
+import java.util.ArrayList;
+
+//________CLASSES_______________
+class Connectors {
+	int head;
+	int tail;
+	
+	public Connectors() {
+		
+	}
+	
+	private int assign_pos(ArrayList<Object> list_connectors) {
+		return 0;
+	}
+	
+	public int get_head() {
+		return this.head;
+	}
+	
+	public int get_tail() {
+		return this.tail;
+	}
+}
+
+class Snakes extends Connectors {
+	
+	public Snakes() {
+		
+	}
+}
+
+class Ladders extends Connectors {
+	public Ladders() {
+		
+	}
+}
+
+class Users {
+	int pos;
+	public Users() {
+		
+	}
+	
+	public int get_pos() {
+		return this.pos;
+	}
+}
+
 
 public class Snakes_and_ladders {
 
 	public static void main(String[] args) {
-		System.out.println("test");
+		
+		ArrayList<Connectors> list_connectors = new ArrayList<Connectors>();
+		
+		
+		//System.out.println("test");
 	}
 
 }
