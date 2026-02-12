4609ac2380e4e4f8c45262577dcf520368e12b3f
lakira23 <lakirar2020@gmail.com>
2026-02-12 15:04:38 +0000

    game_status implementation

--- Assignments/Assignment_2_and_3/src/main/java/Snakes_and_ladders.java
+++ Assignments/Assignment_2_and_3/src/main/java/Snakes_and_ladders.java
@@ -1,4 +1,12 @@
+//________imports______________
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+import swiftbot.Button;
+import swiftbot.SwiftBotAPI;
+
 
 //________CLASSES_______________
 class Connectors {
@@ -46,15 +54,52 @@ class Users {
 	}
 }
 
+class Input_Handler{
+	
+	static SwiftBotAPI swiftBot = SwiftBotAPI.INSTANCE;
+	public Input_Handler() {
+		try {
+			swiftBot.disableButton(Button.A);
+			swiftBot.disableButton(Button.B);
+			swiftBot.disableButton(Button.X);
+			swiftBot.disableButton(Button.Y);
+
+			swiftBot.enableButton(Button.A, () -> {userinput.add("Red"); System.out.println("A been pressed");});
+			swiftBot.enableButton(Button.B, () -> {userinput.add("Blue"); System.out.println("B been pressed");});
+			swiftBot.enableButton(Button.X, () -> {userinput.add("Green"); System.out.println("X been pressed");});
+			swiftBot.enableButton(Button.Y, () -> {userinput.add("White"); System.out.println("Y been pressed");});
+
+
+		}
+
+		catch (Exception e) {
+			System.out.println("please enter a correct key!!");
+			
+		}
+		
+	}
+}
 
 public class Snakes_and_ladders {
+	static boolean game_status = true;
+ 
+	
 
 	public static void main(String[] args) {
 		
-		ArrayList<Connectors> list_connectors = new ArrayList<Connectors>();
+		Map<String,Boolean> game_screen = new HashMap<>();
 		
+		game_screen.put("Menu", true);
+		game_screen.put("Game", false);
+//		System.out.println("Press [Y] in the SwiftBot to start");
+//		
+//		//System.out.println("test");
 		
-		//System.out.println("test");
+		while (game_status) {
+			
+		}
 	}
+	
+	
 
 }
