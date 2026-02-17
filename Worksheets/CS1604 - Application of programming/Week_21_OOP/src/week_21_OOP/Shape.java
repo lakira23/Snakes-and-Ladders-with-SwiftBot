1171104f8e70a4fa253daa0d04b339de5e27a20f
lakira23 <lakirar2020@gmail.com>
2026-02-17 11:30:47 +0000

    testing error fix

--- Worksheets/CS1604 - Application of programming/Week_21_OOP/src/week_21_OOP/Shape.java
+++ Worksheets/CS1604 - Application of programming/Week_21_OOP/src/week_21_OOP/Shape.java
@@ -6,6 +6,7 @@ public class Shape {
 		System.out.println("Circle area = "+3.14*r*r);
 	}
 	public void area(double b, double h) {
+		
 		System.out.println("Triangle area="+0.5*b*h);
 	}
 	public void area(int l, int b) {
